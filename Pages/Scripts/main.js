eb974dedf7db8aa2f4e7c831a6489d8bfa1256fb
zgardner11 <zgardner.cinema@gmail.com>
2022-10-05 21:25:44 -0700

    initial ideas

---
+++ Pages/Scripts/main.js
@@ -0,0 +1,55 @@
+//Hamburger menu for mobile navigation
+
+const burger = document.querySelector('.burger');
+const navMenu = document.querySelector('.navlinks-menu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function isBigPenis(){
+    let bigPenis = false;
+    if(bigPenis != true){
+        return 'getwrektnerd';
+    }
+}
+
+console.log(isBigPenis());
